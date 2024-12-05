dcb2d69e5ac3f4b9a62f605da664d381b8e6fe16
yumarie <yumarie@umich.edu>
2024-12-05 14:02:51 -0500

    grouped circle packing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,37 @@
 	var height = 450;
 
 	onMount(() => {
+		// set the dimensions and margins of the graph
+		var width = 450;
+		var height = 450;
+
 		// append the svg object to the body of the page
 		var svg = d3.select('#cluster').append('svg').attr('width', 450).attr('height', 450);
 
 		// create dummy data -> just one element per circle
 		var data = [
-			{ name: 'A' },
-			{ name: 'B' },
-			{ name: 'C' },
-			{ name: 'D' },
-			{ name: 'E' },
-			{ name: 'F' },
-			{ name: 'G' },
-			{ name: 'H' }
+			{ name: 'A', group: 1 },
+			{ name: 'B', group: 1 },
+			{ name: 'C', group: 1 },
+			{ name: 'D', group: 1 },
+			{ name: 'E', group: 1 },
+			{ name: 'F', group: 1 },
+			{ name: 'G', group: 2 },
+			{ name: 'H', group: 2 },
+			{ name: 'I', group: 2 },
+			{ name: 'J', group: 2 },
+			{ name: 'K', group: 2 },
+			{ name: 'L', group: 2 },
+			{ name: 'M', group: 3 },
+			{ name: 'N', group: 3 },
+			{ name: 'O', group: 3 }
 		];
+
+		// A scale that gives a X target position for each group
+		var x = d3.scaleOrdinal().domain([1, 2, 3]).range([50, 200, 340]);
+
+		// A color scale
+		var color = d3.scaleOrdinal().domain([1, 2, 3]).range(['#F8766D', '#00BA38', '#619CFF']);
 
 		// Initialize the circle: all located at the center of the svg area
 		var node = svg
@@ -28,17 +45,34 @@
 			.data(data)
 			.enter()
 			.append('circle')
-			.attr('r', 25)
+			.attr('r', 29)
 			.attr('cx', width / 2)
 			.attr('cy', height / 2)
-			.style('fill', '#69b3a2')
-			.style('fill-opacity', 0.3)
-			.attr('stroke', '#69a2b2')
-			.style('stroke-width', 4);
-
+			.style('fill', function (d) {
+				return color(d.group);
+			})
+			.style('fill-opacity', 0.8)
+			.attr('stroke', 'black')
+			.style('stroke-width', 4)
 		// Features of the forces applied to the nodes:
 		var simulation = d3
 			.forceSimulation()
+			.force(
+				'x',
+				d3
+					.forceX()
+					.strength(0.5)
+					.x(function (d) {
+						return x(d.group);
+					})
+			)
+			.force(
+				'y',
+				d3
+					.forceY()
+					.strength(0.1)
+					.y(height / 2)
+			)
 			.force(
 				'center',
 				d3
@@ -46,8 +80,8 @@
 					.x(width / 2)
 					.y(height / 2)
 			) // Attraction to the center of the svg area
-			.force('charge', d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-			.force('collide', d3.forceCollide().strength(0.01).radius(30).iterations(1)); // Force that avoids circle overlapping
+			.force('charge', d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
+			.force('collide', d3.forceCollide().strength(0.1).radius(32).iterations(1)); // Force that avoids circle overlapping
 
 		// Apply these forces to the nodes and update their positions.
 		// Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
@@ -60,6 +94,7 @@
 					return d.y;
 				});
 		});
+
 	});
 </script>
 
