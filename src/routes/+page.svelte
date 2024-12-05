9b8670ff53879de50d7a0c6dd43b08e26227a77e
yumarie <yumarie@umich.edu>
2024-12-05 11:54:47 -0500

    fix internal error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,66 @@
 <script>
-    import * as d3 from "d3";
-    // set the dimensions and margins of the graph
-    var width = 450
-    var height = 450
-    
-    // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
-      .append("svg")
-        .attr("width", 450)
-        .attr("height", 450)
-    
-    // create dummy data -> just one element per circle
-    var data = [{ "name": "A" }, { "name": "B" }, { "name": "C" }, { "name": "D" }, { "name": "E" }, { "name": "F" }, { "name": "G" }, { "name": "H" }]
-    
-    // Initialize the circle: all located at the center of the svg area
-    var node = svg.append("g")
-      .selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-        .attr("r", 25)
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .style("fill", "#69b3a2")
-        .style("fill-opacity", 0.3)
-        .attr("stroke", "#69a2b2")
-        .style("stroke-width", 4)
-    
-    // Features of the forces applied to the nodes:
-    var simulation = d3.forceSimulation()
-        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-        .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-        .force("collide", d3.forceCollide().strength(.01).radius(30).iterations(1)) // Force that avoids circle overlapping
-    
-    // Apply these forces to the nodes and update their positions.
-    // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-    simulation
-        .nodes(data)
-        .on("tick", function(d){
-          node
-              .attr("cx", function(d){ return d.x; })
-              .attr("cy", function(d){ return d.y; })
-        });
-    
-    
-    </script>
+	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+	// set the dimensions and margins of the graph
+	var width = 450;
+	var height = 450;
+
+	onMount(() => {
+		// append the svg object to the body of the page
+		var svg = d3.select('#cluster').append('svg').attr('width', 450).attr('height', 450);
+
+		// create dummy data -> just one element per circle
+		var data = [
+			{ name: 'A' },
+			{ name: 'B' },
+			{ name: 'C' },
+			{ name: 'D' },
+			{ name: 'E' },
+			{ name: 'F' },
+			{ name: 'G' },
+			{ name: 'H' }
+		];
+
+		// Initialize the circle: all located at the center of the svg area
+		var node = svg
+			.append('g')
+			.selectAll('circle')
+			.data(data)
+			.enter()
+			.append('circle')
+			.attr('r', 25)
+			.attr('cx', width / 2)
+			.attr('cy', height / 2)
+			.style('fill', '#69b3a2')
+			.style('fill-opacity', 0.3)
+			.attr('stroke', '#69a2b2')
+			.style('stroke-width', 4);
+
+		// Features of the forces applied to the nodes:
+		var simulation = d3
+			.forceSimulation()
+			.force(
+				'center',
+				d3
+					.forceCenter()
+					.x(width / 2)
+					.y(height / 2)
+			) // Attraction to the center of the svg area
+			.force('charge', d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
+			.force('collide', d3.forceCollide().strength(0.01).radius(30).iterations(1)); // Force that avoids circle overlapping
+
+		// Apply these forces to the nodes and update their positions.
+		// Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+		simulation.nodes(data).on('tick', function (d) {
+			node
+				.attr('cx', function (d) {
+					return d.x;
+				})
+				.attr('cy', function (d) {
+					return d.y;
+				});
+		});
+	});
+</script>
+
+<div id="cluster"></div>
